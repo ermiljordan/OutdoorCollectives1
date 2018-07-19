8f04afc04a703353553cf7759b88f4440aa7ff5e
chiwwy <ermiljordan64@gmail.com>
2018-07-19 19:50:00 +0900

    style tablet

--- themes/odc/js/script.js
+++ themes/odc/js/script.js
@@ -1,14 +1,15 @@
-(function($) {
-  $('#subscribe').on('click', function() {
-    event.preventDefault();
-    $('.wpcf7-email').toggleClass('search-visble');
-  })
+// (function($) {
+//   $('#subscribe').on('click', function() {
+//     event.preventDefault();
+//     $('.wpcf7-email').toggleClass('search-visble');
+//     $('.wpcf7-email').focus();
+//   })
 
-  // $('.wpcf7-submit').on('blur keyup', function() {
-  //   if($('.wpcf7-email').valid()) {
-  //     $('.wpcf7-submit').prop('disabled', false);
-  //   } else {
-  //     $('.wpcf7-submit').prop('disabled', 'disabled');
-  //   }
-  // })
-})(jQuery);
+//   // $('.wpcf7-submit').on('blur keyup', function() {
+//   //   if($('.wpcf7-email').valid()) {
+//   //     $('.wpcf7-submit').prop('disabled', false);
+//   //   } else {
+//   //     $('.wpcf7-submit').prop('disabled', 'disabled');
+//   //   }
+//   // })
+// })(jQuery);
